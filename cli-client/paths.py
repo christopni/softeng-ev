88c2bfe8c3ecb2bc1525cc9d03db39c0ff41e031
Nikolaos Christopoulos <75259526+christopni@users.noreply.github.com>
2021-04-19 21:55:39 +0300

    Update paths.py

--- cli-client/paths.py
+++ cli-client/paths.py
@@ -1,7 +1,7 @@
 #PATHS
 
 baseURL='http://localhost:8765/evcharge/api'
-folder='C:/Users/nikos/softeng-cli/cli/'
+folder=''
 token_path=f'{folder}softeng20bAPI.token'
 temporary_path = 'f{folder}temporary.csv'
 
